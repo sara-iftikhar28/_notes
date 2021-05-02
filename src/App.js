d2e28dfd16953e8a0700130eb62f46080bee407b
Sara Iftikhar <siftikhar@ssidecisions.com>
2021-05-03 00:22:32 +0500

    Crud operations for notes added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+import AddNote from './components/AddNote/AddNote'
+import ListNote from './components/ListNote/ListNote'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import EditNote from './components/EditNote/EditNote';
+import Header from './components/Navbar/Navbar'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor() {
+    super()
+    this.state = {
+      notes: [
+        {title:'title1',detail:'detail1'},
+        {title:'title2',detail:'detail2'},
+        {title:'title3',detail:'detail3'},
+        {title:'title4',detail:'detail4'},
+      ],
+      selectedIndex:null
+    }
+  }
+
+  addNote = (newNote)=>{
+    this.setState(prevState => ({
+      notes: [...prevState.notes, newNote],
+    }));
+  }
+
+  selectNote = (index)=>{
+    this.setState({
+      selectedIndex:index
+    })
+  }
+
+  updateNote = (note)=>{
+    let notes = this.state.notes
+    notes[this.state.selectedIndex] = note
+    this.setState({
+      notes:notes
+    })
+  }
+
+  deleteNote =(index) =>  {
+    let notes = [...this.state.notes];
+    notes.splice(index, 1);
+    this.setState({notes: notes});
+  }
+
+  getSeletectedNote= () => {
+    if(this.state.selectedIndex != null){
+      return this.state.notes[this.state.selectedIndex]
+    }
+    return null
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <Container>
+          <Row md={12}>
+            <Col ><AddNote addNoteHandler={this.addNote} /></Col>
+            <Col ><ListNote notes={this.state.notes} selectNote={this.selectNote} selectedIndex={this.state.selectedIndex} deleteNote={this.deleteNote}/></Col>
+              {this.state.selectedIndex != null && <Col ><EditNote  note={this.getSeletectedNote()} updateNote={this.updateNote} /></Col>}
+          </Row>
+        </Container>
+      </div>
+
+    );
+  }
 }
 
 export default App;
